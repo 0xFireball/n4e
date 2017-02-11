b8ea55d604b9da76c89f6e69a5f9277dcbdea8ee
0xFireball <0xFireball@outlook.com>
2017-02-10 16:15:34 -0800

    allow user to set font

--- Sources/n4e/ui/NEText.hx
+++ Sources/n4e/ui/NEText.hx
@@ -1,16 +1,26 @@
 package n4e.ui;
 
+import kha.Color;
+import kha.Font;
+
 import n4.entities.NSprite;
 
 class NEText extends NSprite {
-	public function new(X:Float, Y:Float, Text:String, Size:Int) {
+	public static var font(default, default):Font;
+
+	public function new(X:Float, Y:Float, Text:String, ?Size:Int = 16, ?TextColor:Color) {
+		if (TextColor == null) TextColor = Color.White;
 		// construct sprite
 		super(X, Y);
 		// render custom graphic containing text
 		renderGraphic(Std.int(Size * Text.length * 1.5), Std.int(Size * 1.2), function (gpx) {
-			gpx.g2.begin();
-			gpx.g2.drawString(Text, 0, 0);
-			gpx.g2.end();
+			var ctx = gpx.g2;
+			ctx.begin();
+			ctx.color = TextColor;
+			ctx.font = font;
+			ctx.fontSize = Size;
+			ctx.drawString(Text, 0, 0);
+			ctx.end();
 		}, "t_" + Text);
 	}
 }
