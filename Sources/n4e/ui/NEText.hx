cf0e7ef504fd0a723991e85232e9a18d770bab3d
0xFireball <0xFireball@outlook.com>
2017-02-10 16:56:04 -0800

    allow custom font

--- Sources/n4e/ui/NEText.hx
+++ Sources/n4e/ui/NEText.hx
@@ -6,18 +6,19 @@ import kha.Font;
 import n4.entities.NSprite;
 
 class NEText extends NSprite {
-	public static var font(default, default):Font;
+	public static var defaultFont(default, default):Font;
 
-	public function new(X:Float, Y:Float, Text:String, ?Size:Int = 16, ?TextColor:Color) {
+	public function new(X:Float, Y:Float, Text:String, ?Size:Int = 16, ?TextColor:Color, ?Font:Font) {
+		if (Font == null) Font = defaultFont;
 		if (TextColor == null) TextColor = Color.White;
 		// construct sprite
 		super(X, Y);
 		// render custom graphic containing text
-		renderGraphic(Std.int(font.width(Size, Text)) + 1, Std.int(font.height(Size)) + 1, function (gpx) {
+		renderGraphic(Std.int(Font.width(Size, Text)) + 1, Std.int(Font.height(Size)) + 1, function (gpx) {
 			var ctx = gpx.g2;
 			ctx.begin();
 			ctx.color = TextColor;
-			ctx.font = font;
+			ctx.font = Font;
 			ctx.fontSize = Size;
 			ctx.drawString(Text, 0, 0);
 			ctx.end();
